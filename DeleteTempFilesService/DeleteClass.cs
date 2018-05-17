3c9b3e66eeefd58f8abf09f1d7b3b5f1e9dd5a42
Slavena Koleva <slavena_kolevaa@abv.bg>
2018-05-17 13:43:00 +0300

    Add project files.

---
+++ DeleteTempFilesService/DeleteClass.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+namespace DeleteTempFilesService
+{
+    public static class DeleteClass
+    {
+        public static void WriteErrorLog(string message)
+        {
+            StreamWriter streamWriter = null;
+            try
+            {
+                streamWriter = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+                streamWriter.WriteLine(DateTime.Now.ToString() + ": " + message);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+            catch
+            {
+
+            }
+        }
+
+        public static void WriteErrorLog(Exception ex)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\LogFile.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + ": " + ex.Source.ToString().Trim() + "; " + ex.Message.ToString().Trim());
+                sw.Flush();
+                sw.Close();
+            }
+            catch
+            {
+            }
+        }
+
+        public static void DeleteTempFiles(string pathToTempFiles)
+        {
+            try
+            {
+                Directory.Delete(pathToTempFiles, true);
+
+                bool directoryExists = Directory.Exists(pathToTempFiles);
+                WriteErrorLog("Temp directory exists? - " + directoryExists);
+            }
+            catch (Exception e)
+            {
+                WriteErrorLog(e);
+            }
+        }
+    }
+}
---
+++ DeleteTempFilesService/Sheduler.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.ServiceProcess;
+using System.Timers;
+
+namespace DeleteTempFilesService
+{
+    public partial class Sheduler : ServiceBase
+    {
+        private Timer timer = null;
+        private bool executed;
+        private DateTime executeDate;
+
+        public Sheduler()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            executeDate = DateTime.Now;
+            executed = false;
+            timer = new Timer();
+            timer.AutoReset = true;
+            // The interval is calculated in milliseconds.
+            timer.Interval = Properties.Settings.Default.TimerInterval * 60 * 1000;
+            // Add an event to the timer that will perform the DeleteTempFiles() method.
+            timer.Elapsed += new ElapsedEventHandler(this.timer_Tick);
+            timer.Start();
+
+            DeleteClass.WriteErrorLog("####### SERVICE STARTED #######");
+        }
+
+        private void timer_Tick(object sender, ElapsedEventArgs e)
+        {
+            if (DateTime.Today > executeDate)
+            {
+                executeDate = DateTime.Today;
+                executed = false;
+            }
+            // If the current hour equals to the number, set in Settings and if the deletion is not executed yet,
+            // delete the temp files.
+            if (!executed && DateTime.Now.Hour == Properties.Settings.Default.ExecuteTime)
+            {
+                timer.Stop();
+                DeleteClass.WriteErrorLog("Delete temp files started: " + DateTime.Now.ToString());
+                string tempPath = Environment.GetEnvironmentVariable("TEMP");
+                DeleteClass.DeleteTempFiles(tempPath);
+                
+                timer.Start();
+            }
+        }
+
+        protected override void OnStop()
+        {
+            timer.Stop();
+            DeleteClass.WriteErrorLog("####### SERVICE STOPPED #######");
+        }
+    }
+}
